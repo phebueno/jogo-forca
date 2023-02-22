263907f04307e9da742efb6128f75957452338ca
Phebueno <fcbgomes@gmail.com>
2023-02-22 15:07:45 -0300

    Adicionados atributos para correção automática

--- src/components/Jogo/Jogo.js
+++ src/components/Jogo/Jogo.js
@@ -5,13 +5,12 @@ export default function Jogo({ imgForca, palavras, arrPalavraEscondida, escolher
   //Adicionar duas linhas com as condições de erro/acerto para enfiar no código abaixo: 
   const acerto = acertos===(arrPalavraEscondida.filter(apenasUnicos).length) && "acerto";
   const erro = tentativas===6 && "erro";
-  console.log(tentativas);
   return (
     <main className="jogo">
-      <img src={imgForca} alt="" />
+      <img data-test="game-image" src={imgForca} alt="" />
       <section className="lado-palavra">
-        <button className="botao-palavra" onClick={()=>escolherPalavra(palavras)}>Escolher Palavra</button>
-        <p className={`${acerto} ${erro}`}>{arrPalavraEscondida.map((letra)=>(letrasEscolhidas.includes(letra) ? letra : " _"))}</p>
+        <button data-test="choose-word" className="botao-palavra" onClick={()=>escolherPalavra(palavras)}>Escolher Palavra</button>
+        <p data-test="word" className={`${acerto} ${erro}`}>{arrPalavraEscondida.map((letra)=>(letrasEscolhidas.includes(letra) ? letra : " _"))}</p>
       </section>
     </main>
   );
--- src/components/Letras/Letras.js
+++ src/components/Letras/Letras.js
@@ -1,13 +1,11 @@
 import alfabeto from "./alfabeto";
 
-export default function Letras({letrasEscolhidas, escolherLetra, arrPalavraEscondida}) { 
+export default function Letras({letrasEscolhidas, escolherLetra}) { 
   return (
     <div className="letras-box">
         {alfabeto.map((letra,index)=>(
-            <button key={index} className={`letra-box ${letrasEscolhidas.includes(letra) ? "letra-escolhida":""}`} onClick={()=>escolherLetra(letra)} disabled={letrasEscolhidas.includes(letra) && true }>{letra.toUpperCase()}</button>
+            <button data-test="letter" key={index} className={`letra-box ${letrasEscolhidas.includes(letra) ? "letra-escolhida":""}`} onClick={()=>escolherLetra(letra)} disabled={letrasEscolhidas.includes(letra) && true }>{letra.toUpperCase()}</button>
         ))}  
     </div>
   );
 }
-
-// disabled={letrasEscolhidas.includes(letra) && true }
