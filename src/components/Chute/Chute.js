b9d4890629eda3d5151f35f666fd1e6c9c730103
Phebueno <fcbgomes@gmail.com>
2023-02-23 16:29:24 -0300

    Versão para autoavaliação

--- src/components/Chute/Chute.js
+++ src/components/Chute/Chute.js
@@ -4,8 +4,8 @@ export default function Chute({chute, setChute, chutar, letrasEscolhidasNum, let
   return (
     <div className="chute">
       <p>Já sei a palavra!</p>
-      <input className={inicioDeJogo} type="text" onChange={(e)=>setChute(e.target.value)} onKeyDown={(e)=> e.key ==='Enter' && chutar()} value={chute} disabled={inicioDeJogo}/>
-      <button className={`chutar-palavra ${inicioDeJogo}`} onClick={chutar} disabled={inicioDeJogo}>Chutar</button>
+      <input data-test="guess-input" className={inicioDeJogo} type="text" onChange={(e)=>setChute(e.target.value)} onKeyDown={(e)=> e.key ==='Enter' && chutar()} value={chute} disabled={inicioDeJogo}/>
+      <button data-test="guess-button" className={`chutar-palavra ${inicioDeJogo}`} onClick={chutar} disabled={inicioDeJogo}>Chutar</button>
     </div>
   );
 }
